e2efb05b9117fe6488d47835e5036691865085b4
TOUR-ALEX\Alexandre <fantazacmec1@hotmail.com>
2018-10-11 16:05:43 -0400

    Debut de collision

--- Simulation/Assets/Scripts/BallCollisionDetector.cs
+++ Simulation/Assets/Scripts/BallCollisionDetector.cs
@@ -2,13 +2,44 @@
 
 public class BallCollisionDetector : MonoBehaviour
 {
+    private float timeSinceCreation;
+    private Vector3 speed;
+    private float initialYSpeed;
+
+    private const float ACCELERATION = -9.8f;
+    private const float SPEED_PERCENTAGE_KEPT_ON_COLLISION_WITH_FLOOR = 0.9f;
+
+    private void Start()
+    {
+        speed = Vector3.right * Random.Range(1f, 3f) + Vector3.up * Random.Range(2f, 5f) + Vector3.forward * Random.Range(-2f, 2f);
+        initialYSpeed = speed.y;
+        Debug.Log(speed);
+    }
+
     private void Update()
     {
         Collider[] colliders = Physics.OverlapSphere(transform.position, transform.localScale.x * 0.5f);
-        if (colliders.Length > 0)
+        foreach (Collider collider in colliders)
         {
-            Debug.Log(colliders[0].gameObject.name);
+            if (collider is BoxCollider)
+            {
+                timeSinceCreation = 0;
+                initialYSpeed = -speed.y * SPEED_PERCENTAGE_KEPT_ON_COLLISION_WITH_FLOOR;
+                speed = new Vector3(speed.x, initialYSpeed, speed.z);
+                Debug.Log(speed);
+            }
+            else if (collider is SphereCollider && collider.GetComponent<MeshRenderer>().material.color != GetComponent<MeshRenderer>().material.color)
+            {
+                GetComponent<MeshRenderer>().material.color = collider.GetComponent<MeshRenderer>().material.color;
+            }
         }
-        transform.position += Vector3.down * 0.01f;
+        timeSinceCreation += Time.deltaTime;
+        speed = new Vector3(speed.x, calculateYSpeed(), speed.z);
+        transform.position += speed / 60f;
+    }
+
+    private float calculateYSpeed()
+    {
+        return initialYSpeed + ACCELERATION * timeSinceCreation;
     }
 }
