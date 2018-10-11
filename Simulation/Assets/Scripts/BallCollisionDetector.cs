b61c0d337d06f7c8df67daf1d932ef2e1a8a775e
TOUR-ALEX\Alexandre <fantazacmec1@hotmail.com>
2018-10-11 15:36:46 -0400

    Debut collision

---
+++ Simulation/Assets/Scripts/BallCollisionDetector.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class BallCollisionDetector : MonoBehaviour
+{
+    private void Update()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, transform.localScale.x * 0.5f);
+        if (colliders.Length > 0)
+        {
+            Debug.Log(colliders[0].gameObject.name);
+        }
+        transform.position += Vector3.down * 0.01f;
+    }
+}
